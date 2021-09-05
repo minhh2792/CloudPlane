a2144250d1de67c5c0ec615ce402d8f40352a2e1
booky10 <boooky10@gmail.com>
2021-09-05 18:30:17 +0200

    updated update upstream script

--- scripts/updateUpstream.sh
+++ scripts/updateUpstream.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 
+# Exit instantly if any command fails
+set -e
+
 # The root directory for the project
 ROOT_DIR="$(dirname $0)/.."
-echo "Project root directory detected as $ROOT_DIR"
+echo "Project root directory detected as $ROOT_DIR."
 
-# This can be adjusted to the wanted repository, branch and gradle property
-GRADLE_PROPERTY="purpurRef"
-REPOSITORY="pl3xgaming/Purpur"
-BRANCH="ver/1.17.1"
+# Extract the github organisation and repository from the gradle buildscript
+ORGANISATION=$(cat build.gradle.kts | grep 'set(github' | awk -F '"' '{ print $2}')
+REPOSITORY=$(cat build.gradle.kts | grep 'set(github' | awk -F '"' '{ print $4}')
+
+# Query the default branch from the github api
+BRANCH=$(curl -s "https://api.github.com/repos/$ORGANISATION/$REPOSITORY" | jq -r .default_branch)
+
+# Extract the current gradle property from the buildscript
+GRADLE_PROPERTY=$(cat build.gradle.kts | grep gradleProperty | awk -F '\"' '{ print $2}')
 
 # Print out the information which will be used for updating to the runner.
-echo "Will be updating property $GRADLE_PROPERTY from repository $REPOSITORY in branch $BRANCH."
+echo "Will be updating property $GRADLE_PROPERTY from repository $ORGANISATION/$REPOSITORY in branch $BRANCH."
 
 # Curl the branch information and parse the commit hash using python
-LATEST_COMMIT=$(curl -s "https://api.github.com/repos/$REPOSITORY/branches/$BRANCH" | python -c 'import sys, json; print(json.load(sys.stdin)["commit"]["sha"])')
-echo "Curled latest commit hash of $REPOSITORY in $BRANCH: $LATEST_COMMIT."
+LATEST_COMMIT=$(curl -s "https://api.github.com/repos/$ORGANISATION/$REPOSITORY/branches/$BRANCH" | jq -r .commit.sha)
+echo "Curled latest commit hash of $ORGANISATION/$REPOSITORY in $BRANCH: $LATEST_COMMIT."
 
 # Replace in the gradle.properties file the old commit hash with the new one
-perl -pi -e "s/$GRADLE_PROPERTY = [0-9a-z]{40}/$GRADLE_PROPERTY = $LATEST_COMMIT/g" "$ROOT_DIR/gradle.properties"
+sed -ri "s/($GRADLE_PROPERTY ?= ?)[0-9a-z]{40}/\1$LATEST_COMMIT/g" "$ROOT_DIR/gradle.properties"
 echo "Replaced old commit hash in gradle.properties with $LATEST_COMMIT."
