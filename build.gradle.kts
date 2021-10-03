a326e5679993c0dd35d3cd6d5bbb5bb2ae5bf700
booky10 <boooky10@gmail.com>
2021-10-03 11:04:43 +0200

    updated paperweight to 1.1.12 and cleaned up the buildscript

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,24 +3,10 @@ plugins {
     java
 
     id("com.github.johnrengelman.shadow") version "7.0.0" apply false
-    id("io.papermc.paperweight.patcher") version "1.1.11"
+    id("io.papermc.paperweight.patcher") version "1.1.12"
 }
 
-repositories {
-    mavenCentral()
-    maven("https://papermc.io/repo/repository/maven-public/") {
-        content {
-            onlyForConfigurations("paperclip")
-        }
-    }
-}
-
-dependencies {
-    remapper("org.quiltmc:tiny-remapper:0.4.3:fat")
-    paperclip("io.papermc:paperclip:2.0.1")
-}
-
-subprojects {
+allprojects {
     apply(plugin = "java")
     apply(plugin = "maven-publish")
 
@@ -29,23 +15,44 @@ subprojects {
             languageVersion.set(JavaLanguageVersion.of(16))
         }
     }
+}
 
-    tasks.withType<JavaCompile>().configureEach {
-        options.encoding = "UTF-8"
+subprojects {
+    tasks.withType<JavaCompile> {
+        options.encoding = Charsets.UTF_8.name()
         options.release.set(16)
     }
 
+    tasks.withType<Javadoc> {
+        options.encoding = Charsets.UTF_8.name()
+    }
+
+    tasks.withType<ProcessResources> {
+        filteringCharset = Charsets.UTF_8.name()
+    }
+
     repositories {
-        maven("https://oss.sonatype.org/content/repositories/snapshots/")
-        maven("https://hub.spigotmc.org/nexus/content/groups/public/")
-        maven("https://repo.md-5.net/content/repositories/releases/")
-        maven("https://papermc.io/repo/repository/maven-public/")
+        mavenCentral()
         maven("https://oss.sonatype.org/content/groups/public/")
+        maven("https://papermc.io/repo/repository/maven-public/")
         maven("https://ci.emc.gs/nexus/content/groups/aikar/")
         maven("https://repo.aikar.co/content/groups/aikar")
-        maven("https://repo1.maven.org/maven2/")
+        maven("https://repo.md-5.net/content/repositories/releases/")
+        maven("https://hub.spigotmc.org/nexus/content/groups/public/")
         maven("https://jitpack.io")
     }
+}
+
+repositories {
+    mavenCentral()
+    maven("https://papermc.io/repo/repository/maven-public/") {
+        content { onlyForConfigurations("paperclip") }
+    }
+}
+
+dependencies {
+    remapper("net.fabricmc:tiny-remapper:0.6.0:fat")
+    paperclip("io.papermc:paperclip:2.0.1")
 }
 
 paperweight {
@@ -58,7 +65,7 @@ paperweight {
         withStandardPatcher {
             baseName("Airplane")
 
-            remapRepo.set("https://maven.quiltmc.org/repository/release/")
+            remapRepo.set("https://maven.fabricmc.net/")
             decompileRepo.set("https://files.minecraftforge.net/maven/")
 
             apiOutputDir.set(layout.projectDirectory.dir("CloudPlane-API"))
