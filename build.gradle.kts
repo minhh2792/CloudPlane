9498b652454b702d0ff75191843b0482fcc7526f
booky10 <boooky10@gmail.com>
2021-12-04 15:51:05 +0100

    Updated to minecraft 1.18, temporarily changed upstream to pufferfish

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,46 +1,8 @@
 plugins {
-    idea
-    java
+    `java-library`
 
-    id("com.github.johnrengelman.shadow") version "7.1.0" apply false
-    id("io.papermc.paperweight.patcher") version "1.1.14"
-}
-
-allprojects {
-    apply(plugin = "java")
-    apply(plugin = "maven-publish")
-
-    java {
-        toolchain {
-            languageVersion.set(JavaLanguageVersion.of(16))
-        }
-    }
-}
-
-subprojects {
-    tasks.withType<JavaCompile> {
-        options.encoding = Charsets.UTF_8.name()
-        options.release.set(16)
-    }
-
-    tasks.withType<Javadoc> {
-        options.encoding = Charsets.UTF_8.name()
-    }
-
-    tasks.withType<ProcessResources> {
-        filteringCharset = Charsets.UTF_8.name()
-    }
-
-    repositories {
-        mavenCentral()
-        maven("https://oss.sonatype.org/content/groups/public/")
-        maven("https://papermc.io/repo/repository/maven-public/")
-        maven("https://ci.emc.gs/nexus/content/groups/aikar/")
-        maven("https://repo.aikar.co/content/groups/aikar")
-        maven("https://repo.md-5.net/content/repositories/releases/")
-        maven("https://hub.spigotmc.org/nexus/content/groups/public/")
-        maven("https://jitpack.io")
-    }
+    id("com.github.johnrengelman.shadow") version "7.1.0"
+    id("io.papermc.paperweight.patcher") version "1.3.1"
 }
 
 repositories {
@@ -51,22 +13,61 @@ repositories {
 }
 
 dependencies {
-    remapper("net.fabricmc:tiny-remapper:0.6.0:fat")
-    paperclip("io.papermc:paperclip:2.0.1")
+    decompiler("net.minecraftforge:forgeflower:1.5.498.22")
+    remapper("net.fabricmc:tiny-remapper:0.7.0:fat")
+    paperclip("io.papermc:paperclip:3.0.2")
+}
+
+subprojects {
+    apply(plugin = "java-library")
+    apply(plugin = "maven-publish")
+
+    java {
+        withSourcesJar()
+        withJavadocJar()
+
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+
+        toolchain {
+            languageVersion.set(JavaLanguageVersion.of(17))
+        }
+    }
+
+    tasks.withType<JavaCompile> {
+        options.encoding = Charsets.UTF_8.name()
+        options.release.set(17)
+    }
+
+    repositories {
+        mavenCentral()
+
+        maven("https://oss.sonatype.org/content/groups/public/")
+        maven("https://papermc.io/repo/repository/maven-public/")
+        maven("https://ci.emc.gs/nexus/content/groups/aikar/")
+        maven("https://repo.aikar.co/content/groups/aikar")
+        maven("https://repo.md-5.net/content/repositories/releases/")
+        maven("https://hub.spigotmc.org/nexus/content/groups/public/")
+        maven("https://jitpack.io")
+    }
 }
 
 paperweight {
     serverProject.set(project(":CloudPlane-Server"))
+
+    remapRepo.set("https://maven.fabricmc.net/")
+    decompileRepo.set("https://files.minecraftforge.net/maven/")
 
     useStandardUpstream("pufferfish") {
         url.set(github("pufferfish-gg", "Pufferfish"))
         ref.set(providers.gradleProperty("pufferfishRef"))
 
         withStandardPatcher {
-            baseName("Pufferfish")
+            apiSourceDirPath.set("pufferfish-api")
+            serverSourceDirPath.set("pufferfish-server")
 
-            remapRepo.set("https://maven.fabricmc.net/")
-            decompileRepo.set("https://files.minecraftforge.net/maven/")
+            apiPatchDir.set(layout.projectDirectory.dir("patches${File.separator}api"))
+            serverPatchDir.set(layout.projectDirectory.dir("patches${File.separator}server"))
 
             apiOutputDir.set(layout.projectDirectory.dir("CloudPlane-API"))
             serverOutputDir.set(layout.projectDirectory.dir("CloudPlane-Server"))
